f55a7febb5b6d9cc4a44f98f60417ca77ec90215
Miguel <miguel15081998@gmail.com>
2019-10-19 20:27:10 -0500

    Inicio de proyecto

---
+++ src/main/java/com/cruzsolarcliente/CruzSolarClienteApplication.java
@@ -0,0 +1,13 @@
+package com.cruzsolarcliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruzSolarClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruzSolarClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cruzsolarcliente/CruzSolarClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cruzsolarcliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruzSolarClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
